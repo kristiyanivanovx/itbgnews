deadaad0e562acf3a18b2841770a3a57f81972c4
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2021-08-29 19:41:17 +0300

    Initial commit

---
+++ client/pages/index.js
@@ -0,0 +1,24 @@
+import Landing from './landing'
+import Footer from "./footer";
+import Navbar from "./navbar";
+// import UsersList from './usersList'
+
+export async function getStaticProps() {
+    const res = await fetch('http://localhost:5000/api/customers');
+    const users = await res.json();
+
+    return {
+        props: { users }
+    }
+}
+
+export default function Home({ users }) {
+  return (
+    <div>
+        <Navbar />
+        <Landing />
+        <Footer/>
+        //<UsersList users={users} />
+    </div>
+  )
+}
