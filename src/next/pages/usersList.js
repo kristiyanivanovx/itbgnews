2d6e90021d6bbff69d1ea6872a523db6b79a385c
kristiyanivanovx <kristianiva88@gmail.com>
2021-08-31 17:10:41 +0300

    Update usersList.js

--- src/next/pages/usersList.js
+++ src/next/pages/usersList.js
@@ -5,7 +5,7 @@ function UsersList({ users }) {
             <ul>
                 {users.map((user) => (
                     <li key={user.id}>
-                        {user.firstName}&nbsp;-&nbsp;{user.lastName}
+                        {user.id} - {user.firstName} - {user.lastName}
                     </li>
                 ))}
             </ul>
