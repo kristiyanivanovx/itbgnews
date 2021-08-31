88cdbb9efabdf4e6810093633d3c37c29fdfd95d
Pavel <elias5247698@abv.bg>
2021-08-31 19:49:25 +0300

    first try to commit in my branch

--- server/server.js
+++ server/server.js
@@ -2,16 +2,10 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
-app.get('/api/customers', cors(), (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Mary', lastName: 'Jane'},
-  ];
-
-  res.json(customers);
-});
+app.use(cors())
 
 const port = 5000;
 
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
