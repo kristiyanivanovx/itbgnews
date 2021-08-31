9429c59e1a491a6f2e2dafee4d9e81f8bd93e4c3
kristiyanivanovx <kristianiva88@gmail.com>
2021-08-31 18:57:07 +0300

    rename next to client
    rename next to client

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,7 @@
-import '../styles/globals.css';
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-
-config.autoAddCss = false;
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-    // return <Component {...pageProps} />;
-
-    const getLayout = Component.getLayout || ((page) => page);
-    return getLayout(<Component {...pageProps} />);
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
