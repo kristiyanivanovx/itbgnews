ca1bcab64892fcf615787b29f96e8b00639e2164
Pavel <elias5247698@abv.bg>
2021-08-31 20:34:41 +0300

    creating folders for Backend and start making session with passport

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-
+const passport = require('passport')
+const router = express.Router()
 const app = express();
 app.use(cors())
 
@@ -9,3 +10,6 @@ const port = 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 
+app.post('/login-page', (req , res) => {
+    //
+})
