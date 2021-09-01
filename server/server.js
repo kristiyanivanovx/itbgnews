e7260332a2aa22d20960515e2148234feb53745e
Ivelin Ivanov <48382575+Ivelin09@users.noreply.github.com>
2021-09-01 18:54:36 +0300

    Logging system

--- server/server.js
+++ server/server.js
@@ -1,17 +1,50 @@
 const express = require('express');
 const cors = require('cors');
-
+const passport = require('passport')
+const router = express.Router()
 const app = express();
+app.use(cors())
 
-app.get('/api/customers', cors(), (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Mary', lastName: 'Jane'},
-  ];
-
-  res.json(customers);
-});
-const Jora = 2;
+const mongoose = require("mongoose");
 const port = 5000;
+app.use(express.urlencoded({extended: false}))
 
-app.listen(port, () => `Server running on port ${port}`);
+mongoose.connect('mongodb://localhost:27017/db', {useNewUrlParser: true}).then(() => {
+    console.log("SUCCESS");
+});
+
+const schema = new mongoose.Schema({
+    username: String,
+});
+
+const users = mongoose.model('Scehma', schema);
+//users.collection.createIndex({ email: 1 }, {unique: true});
+
+app.get('/',  async (req, res) => {
+    res.sendFile(__dirname + "\\index.html");
+
+    users.find({} , (err, q) => {
+        console.log("test");
+        if(err) console.log(err);
+        q.map(function(user) {
+            console.log(user);
+
+        });
+
+    });
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+app.post('/login', async (req , res) => {
+    let name = req.body.username;
+    console.log(name);
+    const user = new users({username: name});
+
+    user.save().then(doc => {
+        console.log("SUCCESS: " + doc);
+    }).catch(err => {
+        console.log("ERROR:" + err);
+    });
+})
