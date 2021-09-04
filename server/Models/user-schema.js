784819f417abf1042d3a48052b41e4cd6a53a2f7
Pavel <elias5247698@abv.bg>
2021-09-05 02:40:01 +0300

    testing sessions with passport-local

--- server/Models/user-schema.js
+++ server/Models/user-schema.js
@@ -1,16 +1,32 @@
-const mongoose = require('mongoose')
-const Schema = require("mongoose").Schema
-
-
-mongoose.connect('mongodb://localhost:27017/Hackernews', () => {
-    console.log('Connection ice')
-});
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose
+   .connect('mongodb://localhost:27017/hackerNews')
+   .then((er) => console.log('fine'))
+   .catch((er) => console.log('not fine'));
 
 const userSchema = new Schema({
-    username: String,
-    password: String,
-    date: Date.now(),
-})
+   email: String,
+   username: String,
+   password: String,
+   date: Date
+});
 
+userSchema.index(
+   {
+      email: 1
+   },
+   {
+      unique: true
+   }
+);
+userSchema.index(
+   {
+      username: 1
+   },
+   {
+      unique: true
+   }
+);
 
-module.exports = mongoose.model('userModel', userSchema)
+module.exports = mongoose.model('userModel', userSchema);
--- server/server.js
+++ server/server.js
@@ -1,78 +1,22 @@
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const session = require('express-session')
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const session = require('express-session');
+const usersSchema = require('./models/user-schema');
+const initializePassport = require('./utils/passport-config');
+const routes = require('./routes/login-route');
 
-const initializePassport = require('./utils/passport-config')
-initializePassport(
-    passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-)
-
-const users = []
-
-app.set('view-engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-
-app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', { name: req.user.name })
-})
-
-app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
-})
-
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
-
-app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('register.ejs')
-})
-
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        res.redirect('/login')
-    } catch {
-        res.redirect('/register')
-    }
-})
-
-app.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/login')
-})
-
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next()
-    }
-
-    res.redirect('/login')
-}
-
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect('/')
-    }
-    next()
-}
-
-app.listen(3000)
+initializePassport(passport);
+// all middlewares needed for app
+app.use(
+   session({
+      secret: '12',
+      resave: false,
+      saveUninitialized: false
+   })
+);
+app.use(session());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('', routes);
+app.listen(5000);
