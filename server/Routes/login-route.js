931f845309afb0b8535c1f510e0f13d85bbefa9f
Pavel <elias5247698@abv.bg>
2021-09-05 02:40:01 +0300

    testing sessions with passport-local

--- server/Routes/login-route.js
+++ server/Routes/login-route.js
@@ -1,23 +1,55 @@
-const express = require('express')
-const router = express.Router()
+const passport = require('passport');
+const express = require('express');
+const userSchema = require('../models/user-schema');
+const router = express.Router();
+const { hash } = require('bcrypt');
+router.use(express.json());
 
-router.get('/', (req , res) => {
-    // here we need to take news from database
+router.get('/', isAuthenticated, (req, res) => {
+   console.log(req.isAuthenticated());
+   res.send('not working');
+});
 
-})
+router.get(
+   '/login',
+   passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/register'
+   }),
+   (req, res) => {}
+);
 
-router.get('/login',(req , res) => {
-    res.send('It works for now')
-})
+// perfect
+router.post('/register', async (req, res) => {
+   const { email, password, username } = req.body;
+   const encryptedPassword = await hash(password, 10);
+   try {
+      const newUser = new userSchema({
+         username,
+         email,
+         password: encryptedPassword,
+         date: Date.now()
+      });
+      await newUser.save();
+      res.json({ message: 'success', data: 'it gets login successfully' });
+   } catch (er) {
+      res.json({
+         message: 'error',
+         data: 'This email or the username is already taken'
+      });
+   }
+});
 
-router.post('/login', (req , res) => {
-    const data = req.body.params
-    req.isAuthenticated()
-    // here we need to take data to database and use middleware for sessions
-})
+router.get('/register', (req, res) => {
+   console.log(req.isAuthenticated());
+   res.send('fine');
+});
 
+module.exports = router;
 
-
-
-router.get
-module.exports = router
+function isAuthenticated(req, res, done) {
+   if (req.isAuthenticated()) {
+      done();
+   }
+   res.redirect('/login');
+}
--- server/utils/passport-config.js
+++ server/utils/passport-config.js
@@ -1,38 +1,40 @@
-const LocalStrategy = require('passport').Strategy
-const bcrypt = require('bcrypt')
-const userSchema = require('../Models/user-schema')
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const userSchema = require('../models/user-schema');
 // initialize functions for getting email and id from database will be created later
 // this function applies to be in session if you get
-function initialize(passport, getUserByEmail, getUserById) {
-    const authenticateUser = async (email, password, done) => {
-        // this function needs Mongo DB
-        const user = getUserByEmail(email)
-        if (user == null) {
-            return done(null, false, { message: 'No user with that email' })
-        }
-        try {
-            if (await bcrypt.compare(password, user.password)) {
-                return done(null, user)
-            } else {
-                return done(null, false, { message: 'Password incorrect' })
-            }
-        } catch (e) {
-            return done(e)
-        }
-    }
-    const localStrategy = new LocalStrategy({ usernameField: 'email' })
-    passport.use(localStrategy, authenticateUser)
-    passport.serializeUser((user, done) => done(null, user.id))
-    passport.deserializeUser((id, done) => {
-        return done(null, getUserById(id))
-    })
+function initialize(passport) {
+   const authenticateUser = async (email, password, done) => {
+      const user = await userSchema.findOne({ email });
+      if (user == null) {
+         return done(null, false, { message: 'No user with that email' });
+      }
+      console.log(user.password);
+      console.log(password);
+      try {
+         if (bcrypt.compare(password, user.password)) {
+            console.log('Everything is great');
+            return done(null, user);
+         } else {
+            return done(null, false, { message: 'Password incorrect' });
+         }
+      } catch (e) {
+         return done(e);
+      }
+   };
+   passport.use(
+      new LocalStrategy(
+         {
+            usernameField: 'email',
+            passwordField: 'password'
+         },
+         authenticateUser
+      )
+   );
+   passport.serializeUser((user, done) => done(null, user._id));
+   passport.deserializeUser((id, done) => {
+      return done(null, userSchema.findById({ id }));
+   });
 }
 
-module.exports = initialize
-
-function getUserByEmail(email){
-    userSchema.findOne({'email' : email}, (error, person) => {
-        if(error) return handleError(error)
-
-    })
-}
+module.exports = initialize;
