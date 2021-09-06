7569e8990994fec9716317ad0800e0d81df0f331
MariinaKP <pironevamarina@gmail.com>
2021-09-06 12:50:56 +0300

    added login page, register page and create page

--- client/pages/login.js
+++ client/pages/login.js
@@ -4,7 +4,25 @@ import React, { useState } from 'react';
 const Login = () => {
     return (
        <>
+        <div className={"auth"}>
+            <h2 className={"auth__title"}>Вход</h2>
+            <form className={"auth__input"}>
+                <input className={"user__input"} placeholder={"Е-мейл"}/>
+                <input className={"user__input"} placeholder={"Парола"}/>
+                <button className={"auth__btn"}>
+                        <div className={"auth__btn--background"}>
+                        </div>
+                        <div className={"auth__btn--shadow"}>
 
+                        </div>
+                        <div className={"auth__btn--text"}>Влез</div>
+                </button>
+                <div className={"auth__links"}>
+                <span className={"auth__link"}>Нямаш профил?</span>
+                <span className={"auth__link"}>Забравена парола?</span>
+                </div>
+            </form>
+        </div>
        </>
     );
 };
--- client/pages/register.js
+++ client/pages/register.js
@@ -1,71 +1,25 @@
-import styles from '../styles/Layout.module.css';
 import React, { useState } from 'react';
-import Link from 'next/link';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import HeadComponent from '../components/HeadComponent';
 
 const Register = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordConfirm, setPasswordConfirm] = useState('');
-
-    const submitForm = async () => {
-        await fetch('http://localhost:5000/api/register', {
-            method: 'POST',
-            body: JSON.stringify({ username, password, passwordConfirm }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-    };
-
     return (
-        <div>
-            <HeadComponent currentPageName={'Register'} />
-            <Navbar />
-            <div className="register-page">
-                <div className="form">
-                    <div className="login">
-                        <div className="login-header">
-                            <h1 className={styles.title}>
-                                <Link href={'/register'}>
-                                    <a>Register</a>
-                                </Link>
-                            </h1>
-                            <p>Please enter your credentials to register.</p>
-                        </div>
+    <>
+        <div className={"auth"}>
+            <h2 className={"auth__title"}>Регистрация</h2>
+            <form className={"auth__input"}>
+                <input className={"user__input"} placeholder={"Име"}/>
+                <input className={"user__input"} placeholder={"Е-мейл"}/>
+                <input className={"user__input"} placeholder={"Парола"}/>
+                <button className={"auth__btn"}>
+                    <div className={"auth__btn--background"}>
                     </div>
-                    <input
-                        type="text"
-                        name="username"
-                        placeholder="Username"
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        name="confirm-password"
-                        placeholder="Confirm Password"
-                        onChange={(e) => setPasswordConfirm(e.target.value)}
-                    />
-                    <button onClick={submitForm}>Register</button>
-                    <p className="message">
-                        You have already registered?
-                        <Link href={'/login'}>
-                            <a> Login</a>
-                        </Link>
-                    </p>
-                </div>
-            </div>
-            <Footer />
-        </div>
-    );
-};
+                    <div className={"auth__btn--shadow"}>
 
+                    </div>
+                    <div className={"auth__btn--text"}>Регистрация</div>
+                </button>
+            </form>
+        </div>
+    </>
+    )
+}
 export default Register;
