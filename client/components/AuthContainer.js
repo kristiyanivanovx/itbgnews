b61265a182e3e21f4d2b956f6a0677c1dee5bca9
ee <kristianiva88@gmail.com>
2021-09-09 14:31:36 +0300

    add auth container component
    add auth container component

---
+++ client/components/AuthContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/Auth.module.css';
+
+const AuthContainer = ({ children }) => {
+    return <div className={styles.auth__container}>{children}</div>;
+};
+
+export default AuthContainer;
