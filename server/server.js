45c549f68dc0e0ad62e7966d115f9100abfaa7b3
ee <kristianiva88@gmail.com>
2021-09-09 14:31:36 +0300

    add auth container component
    add auth container component

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
+const cors = require('cors');
+const express = require('express');
+
 require('dotenv').config();
 
-const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-const auth_routes = require('./routes/auth-route');
-app.use('', auth_routes);
+// const auth_routes = require("./routes/auth-route");
+// app.use("", auth_routes);
 
 app.listen(5000, () => {
-   console.log(`Listening on port ${process.env.BACKEND_PORT}`);
+    console.log(`Listening on port ${process.env.BACKEND_PORT}`);
 });
