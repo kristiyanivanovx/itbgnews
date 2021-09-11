0759a2021ff236ae5c3c52179194f6bb58b08091
ee <kristianiva88@gmail.com>
2021-09-12 02:05:45 +0300

    add logout confirmation, handle access and refresh tokens, create cookies, validate that cookies exist on specific routes, code cleanup
    add logout confirmation, handle access and refresh tokens, create cookies, validate that cookies exist on specific routes, code cleanup

--- client/pages/myProfile.js
+++ client/pages/myProfile.js
@@ -5,8 +5,18 @@ import SideNav from '../components/SideNav';
 import Profile from '../components/Profile';
 import HeadComponent from '../components/HeadComponent';
 import getDefaultLayout from '../utilities/getDefaultLayout';
+import { useCookies } from 'react-cookie';
+import Router from 'next/router';
 
-const MyProfile = () => {
+const MyProfile =  () => {
+    const [cookies, setCookie, removeCookie] = useCookies(["access_token", "refresh_token"]);
+
+    // todo: improve checks
+    // if user doesnt have cookies, make him login
+    if (cookies.access_token === undefined || cookies.refresh_token === undefined) {
+        Router.push('/login').then();
+    }
+
     return (
         <>
             <HeadComponent currentPageName={'My Profile'} />
