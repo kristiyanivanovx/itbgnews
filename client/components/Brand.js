fec510db240b946da54c6fad4ee9808fc06a29e7
ee <kristianiva88@gmail.com>
2021-09-14 18:17:56 +0300

    recover deleted workflow processes, resolve merge conflicts
    recover deleted workflow processes, resolve merge conflicts

--- client/components/Brand.js
+++ client/components/Brand.js
@@ -4,35 +4,16 @@ import styles from '../styles/Brand.module.css';
 import React from 'react';
 
 const Brand = () => {
-    return (
-        <div className={styles.brand}>
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <Image
-                className={styles.brand__logo}
-                src={logo}
-                width={'70px'}
-                height={'70px'}
-                alt={'logo'}
-            />
-=======
-=======
->>>>>>> chris
-            <div className={styles.brand__logo}>
-                <Image
-                    src={logo}
-                    alt={'logo'}
-                />
-            </div>
-<<<<<<< HEAD
->>>>>>> Marina
-=======
->>>>>>> chris
-            <div className={styles.brand__title}>
-                <a href="#">IT-BG News</a>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.brand}>
+      <div className={styles.brand__logo}>
+        <Image src={logo} alt={'logo'} />
+      </div>
+      <div className={styles.brand__title}>
+        <a href="#">IT-BG News</a>
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
