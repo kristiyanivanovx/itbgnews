add178d6e2a57945175ac5f49664e2567afa36a1
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-10-02 15:46:49 +0300

    fix

--- frontend-deploy-script.sh
+++ frontend-deploy-script.sh
@@ -1,8 +1,6 @@
 # example deployment script, modify to suit your needs
 
 heroku container:login
-cd "D:\Projects\GitHub repos\itbgnews" || exit
-echo "$PWD"
 docker build -t "itbgnews-client" ./client/ --no-cache
 docker tag itbgnews-client registry.heroku.com/itbgnews/web
 docker push registry.heroku.com/itbgnews/web
