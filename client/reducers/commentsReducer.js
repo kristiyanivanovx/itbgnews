ff257a967a52f2f28852863c8f11dafd71fe2a79
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-10-10 20:54:18 +0300

    feat: start implementing useReducer in Comment

---
+++ client/reducers/commentsReducer.js
@@ -0,0 +1,15 @@
+export const initialState = 0;
+
+const commentsReducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'reset':
+      return initialState;
+    default:
+      return state;
+  }
+};
+export default commentsReducer;
